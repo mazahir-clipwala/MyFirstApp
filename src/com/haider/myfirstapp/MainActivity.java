f0f63a6a072007d4975f2ba1450661ce9282431b
max <clipwala.mazahir@gmail.com>
2015-05-27 20:14:35 -0500

    updated

--- src/com/haider/myfirstapp/MainActivity.java
+++ src/com/haider/myfirstapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.haider.myfirstapp;
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,11 +12,45 @@ import android.widget.EditText;
 public class MainActivity extends ActionBarActivity {
 	
 	public static final String MESSAGE = "com.haider.MainActivity";
+	public static final String TAG = "MyFirstApp";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Log.v (TAG, "onCreate Called");
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.v (TAG, "onStart Called");
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.v (TAG, "onResume Called");
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.v (TAG, "onPause Called");
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.v (TAG, "onStop Called");
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		Log.v (TAG, "onDestroy Called");
 	}
 
 	@Override
